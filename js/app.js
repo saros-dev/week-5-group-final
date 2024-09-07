4a46f218f3ba0c113b6d35424379d109960afa0a
aliap <aliahmadpour1383@gmail.com>
2024-09-07 18:03:10 +0330

    Last

--- js/app.js
+++ js/app.js
@@ -10,3 +10,89 @@ else{
     menuList.style.maxHeight = "0px"
 }
 }
+// const CommentEl = document.querySelector(".swiper-wrapper")
+// async function getdata(){
+//   try {
+//     const response = await fetch("https://dummyjson.com/comments")
+//     const data = await response.json();
+//     let Comments= []
+//     data.comments = Comments
+//     console.log(Comments)
+//     Comments.forEach((item) => {
+//       console.log("ali");
+//       let a=item.comments.body
+//       let b=item.comments.fullname
+//       let c=item.username
+//       console.log(c)
+//       CommentEl.innerHTML+= `<div class="comment-box swiper-slide">
+//       <p class="comment-text">"${a}"</p>
+//       <div class="comment-detail">
+//       <h3>${b}</h3>  
+//       <p>${c}</p>
+//       </div>`
+      
+      
+//     });
+    
+//   } catch (error) {
+//     console.log(error)
+//   }
+// }
+// getdata()
+const CommentEl = document.querySelector(".swiper-wrapper");
+
+async function getdata() {
+  try {
+    const response = await fetch("https://dummyjson.com/comments");
+    const data = await response.json();
+
+    
+    console.log(data);
+
+    
+    data.comments.forEach((item) => {
+      let a = item.body; 
+      let b = item.user.fullName; 
+      let c = item.user.username; 
+
+      
+
+      
+      CommentEl.innerHTML += `
+      <div class="comment-box swiper-slide">
+        <p class="comment-text">"${a}"</p>
+        <div class="comment-detail">
+          <h3>${b}</h3>  
+          <p>@${c}</p>
+        </div>
+      </div>`;
+    });
+
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+getdata();
+
+let swiper = new Swiper('.comments', {
+    // Optional parameters
+
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
